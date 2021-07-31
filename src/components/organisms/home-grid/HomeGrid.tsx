fc974c0bce15c45b83b1b62ba3c0d38e8191b49f
jesus2801 <jesusdavid280147@gmail.com>
2021-07-31 09:32:07 -0500

    spelling corrected

--- src/components/organisms/home-grid/HomeGrid.tsx
+++ src/components/organisms/home-grid/HomeGrid.tsx
@@ -10,15 +10,15 @@ const HomeGrid = () => {
       <HomeCard
         title="Galería"
         button="¡Echa un vistazo!"
-        desc="Observa detalladamente las imagenes de cada uno de los momentos cruciales que desencadenaron esta gran batalla"
+        desc="Observa detalladamente las imágenes de cada uno de los momentos cruciales que desencadenaron esta gran batalla"
         image="/static/cards/batalla-img.jpg"
         link="/gallery"
       />
 
       <HomeCard
         title="Museo"
-        button="¡Describre a estos héroes!"
-        desc="Descubre quienes fueron cada uno de los principales personajes importantes que participaron en el marco de esta batalla de Boyacá"
+        button="¡Descubre a estos héroes!"
+        desc="Descubre quiénes fueron cada uno de los principales personajes importantes que participaron en el marco de esta batalla de Boyacá"
         image="/static/cards/simon-bolivar.webp"
         link="#!"
       />
@@ -26,23 +26,23 @@ const HomeGrid = () => {
       <HomeCard
         title="Lo que nos representa"
         button="¡Veamos esas cualidades!"
-        desc="Los jovenes de 8° y 9° han preparado una sección en donde enmarcan lo que nos representa a nosotros como colombianos"
+        desc="Los jóvenes de 8° y 9° han preparado una sección en donde enmarcan lo que nos representa a nosotros como colombianos"
         image="/static/cards/colombianos.webp"
         link="/videos?v=https://www.youtube.com/embed/jlu9-7JjE78"
       />
 
       <HomeCard
         title="Datos curiosos"
-        button="¡Cuentame más!"
-        desc="Los estudiantes de 7° grado han preparado un serie de sorprendentes datos curiosos sobre nuestro proceso de independencia"
+        button="¡Cuéntame más!"
+        desc="Los estudiantes de 7° grado han preparado una serie de sorprendentes datos curiosos sobre nuestro proceso de independencia"
         image="/static/cards/independencia.jpg"
         link="/videos?v=https://www.youtube.com/embed/jlu9-7JjE78"
       />
 
       <HomeCard
         title="La historia no contada de nuestra independencia"
-        button="¡Pues cuentamela!"
-        desc='Los estudiantes de 11° han elaborado un material audiovisual en donde de manera didactica muestran "Nuestra historia no contada"'
+        button="¡Pues cuéntamela!"
+        desc='Los estudiantes de 11° han elaborado un material audiovisual donde de manera didactica muestran "Nuestra historia no contada"'
         image="/static/cards/independencia-2.jpg"
         link="/videos?v=https://www.youtube.com/embed/jlu9-7JjE78"
       />
